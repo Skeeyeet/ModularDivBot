392061c086d6baa0105ab0a8bbf50ddfe13ca457
Skeeyeet <75297444+Skeeyeet@users.noreply.github.com>
2021-11-01 21:25:57 +1100

    Update CommandHandler.js

--- CommandHandler.js
+++ CommandHandler.js
@@ -41,7 +41,7 @@ const commands = {
 module.exports = async function CommandHandler(message) {
 
     AutomatedSale(message);
-    CheckInvestment(message);
+    //CheckInvestment(message);
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();

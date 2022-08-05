a009b6855ccefbd8b8c5d16ecfbc7989f0cd3fb2
Skeeyeet <75297444+Skeeyeet@users.noreply.github.com>
2022-08-06 00:07:18 +1000

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ client.on('guildMemberSpeaking', async (member, speaking) => {
 
 client.on('presenceUpdate', async (oldPresence, newPresence) => {
     try {
-        if (newPresence.userId === '246807657971712002') {
+        if (newPresence.userId === '254917339810627584') {
             console.log(newPresence.status)
             if (newPresence.status === 'online'){
-                const channel = await client.channels.fetch('736197116996747316')
+                const channel = await client.channels.fetch('783212024808079384')
                 channel.send('hello')
             }
         }        
